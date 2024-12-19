f0e9894859aec5c659338b466a71c5f53590fb45
marvin tan <ymarvintan@gmail.com>
2024-12-19 18:01:47 +0800

    update

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+//allow requests from any origin
+app.use(cors());
 //middleware to parse JSON bodies
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("hello")
+    res.send({message : "hello, this is bookit backend"})
 })
 
 const appointmentRouter = require('./routes/appointments.js');
 
 app.use('/appointments', appointmentRouter)
 
-app.listen(3000)
+app.listen(3001)
