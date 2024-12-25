084111f2c2057aad36b9877fb0e02b311141ffec
marvin <ymarvintan@gmail.com>
2024-12-25 21:18:06 +0800

    update

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -19,21 +19,72 @@ const getLocations = async (req, res) => {
         }
     }
 }
+//get a location
+const getSpecificLocation = async (req, res) => {
+    try{
+        const { location_id } = req.params;
+        //Send GET request to Hapio API
+        const response = await hapioClient.get(`locations/${location_id}`);
+        //Send the response to the frontend
+        res.send(response.data);
 
+    }catch(error){
+        console.error("Error fetching locations", error.message);
+        //handle errors from Hapio API
+        if(error.message){
+            res.status(error.response.status).json(error.response.data);
+        }
+        else{
+            res.status(500).json({ error : "Internal sever error" })
+        }
+    }
+}
 //create a location
 const createLocation = async (req, res) =>{
     try{
-        //Data from the request body
-        const resourceData = req.body;
+        const {
+            name,
+            time_zone,
+            resource_selection_strategy,
+            enabled,
+            postal_code,
+            address,
+            user_id
+        } = req.body;
 
-        //Send POST request to Hapio API with the resource data
-        const response = await hapioClient.post('locations', resourceData);
+        const apiData = {
+            name,
+            time_zone,
+            resource_selection_strategy,
+            enabled,
+        }
 
-        //Send the response back to the frontend
-        res.send(response.data);
+        const response = await hapioClient.post('locations', apiData);
 
-        //After creating a location, use the id to create an location entry in the database.
-    
+        const { id: location_id } = response.data;
+
+        connection.query('INSERT INTO locations (location_id, user_id, name, address, postal_code, timezone) VALUES (?, ?, ?, ?, ?, ?)',
+            [location_id, user_id, name, address, postal_code, time_zone],
+            (err, result) => {
+                if(err){
+                    console.log('Error inserting location into database:', err.message);
+                    return res.status(500).json({ error: 'Database insertion failed' });
+                }
+                console.log('Location entry successfully inserted into database:', result);
+
+                res.status(201).json({
+                    message: 'Location successfully created',
+                    location: {
+                        location_id,
+                        user_id,
+                        name,
+                        address,
+                        postal_code,
+                        time_zone,
+                    },
+                });
+            }
+        )
 
     }catch(error){
         console.error("Error creating resources", error.message);
@@ -48,5 +99,6 @@ const createLocation = async (req, res) =>{
 }
 module.exports = {
     getLocations,
-    createLocation
+    createLocation,
+    getSpecificLocation
 }
--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getLocations, createLocation } = require('../controllers/locationController');
+const { getLocations, createLocation, getSpecificLocation } = require('../controllers/locationController');
 
 router.get('/locations', getLocations);
 router.post('/locations', createLocation);
-
+router.get('/locations/:location_id', getSpecificLocation);
 module.exports = router;
